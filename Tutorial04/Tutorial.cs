c0fa95f0b4868f5e6c11bf60b0b7e0ef57d6aefc
Marc Melvin <marc.melvin@gmail.com>
2011-07-28 20:35:08 -0400

    Added tutorial #4

---
+++ Tutorial04/Tutorial.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace Tutorial04
+{
+    public class Tutorial : GameWindow
+    {
+        const string TITLE = "Tutorial #4";
+        const int WIDTH = 800;
+        const int HEIGHT = 600;
+
+        private float rotation = 0.0f;
+        
+        private int charTextureId;      // Create a private variable to hold our handle to the texture buffer.
+        private float charSize = 64;    // Just a temporary way for us to keep track of how big our caveman is.
+
+        public Tutorial() : base(WIDTH, HEIGHT, GraphicsMode.Default, TITLE) { }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.ClearColor(0.1f, 0.1f, 0.1f, 1f);
+            GL.Enable(EnableCap.Texture2D); // Enable Texture2D to tell OpenGL that it should render bound texture.
+
+            // Load up our image using our LoadTexture() static utility method and save its handle id.
+            charTextureId = Utilities.LoadTexture(@"Images\caveman.png");
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+
+            Matrix4 projection = Matrix4.CreateOrthographic(ClientRectangle.Width, ClientRectangle.Height, -1f, 1f);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projection);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            rotation += (float)e.Time * 360;
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.LoadIdentity();
+            GL.Rotate(rotation, Vector3.UnitZ);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.Begin(BeginMode.Quads);
+
+            GL.BindTexture(TextureTarget.Texture2D, charTextureId); // Bind our texture that we want rendered.
+
+            // Since we are using a texture instead of colors, we can get rid of the Color3() calls and replace
+            // them with TexCoord2() calls. We need to describe what part of the image (in X,Y coordinates from
+            // 0 to 1) correlates to the point we are rendering. The renderer will work out how the image needs
+            // to be drawn for each of the pixels in-between these corner vertices. Additionally, we also
+            // changed our Vertex3() calls to draw the points based on the caveman's width/height.
+            GL.TexCoord2(0f, 1f); GL.Vertex3(-charSize / 2, charSize / 2, 0);
+            GL.TexCoord2(0f, 0f); GL.Vertex3(-charSize / 2, -charSize / 2, 0);
+            GL.TexCoord2(1f, 0f); GL.Vertex3(charSize / 2, -charSize / 2, 0);
+            GL.TexCoord2(1f, 1f); GL.Vertex3(charSize / 2, charSize / 2, 0);
+
+            GL.BindTexture(TextureTarget.Texture2D, 0); // Unbind texture when we are done with it.
+
+            GL.End();
+
+            SwapBuffers();
+        }
+
+        static void Main(string[] args)
+        {
+            using (Tutorial tutorial = new Tutorial())
+            {
+                tutorial.Run(60.0f);
+            }
+        }
+    }
+}
---
+++ Tutorial04/Utilities.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Tutorial04
+{
+    // Utilities will be our static class where we can keep our helper methods.
+    static public class Utilities
+    {
+        // Loading textures takes quite a bit of code, so we throw it in a helper method.
+        static public int LoadTexture(string filename)
+        {
+            // If a filename isn't provided, throw an error.
+            if (String.IsNullOrEmpty(filename))
+                throw new ArgumentException(filename);
+
+            // Generate an OpenGL texture handle and bind to it.
+            int id = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, id);
+
+            // Load up the bitmap image from a file on the computer.
+            Bitmap bmp = new Bitmap(filename);
+
+            // Lock the image's data into system memory so it can be used by OpenGL. We are forcing a
+            // 32-bit alpha/red/green/blue pixel format.
+            BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            // With the image locked, we can tell OpenGL to store the bitmap's scan data into our texture buffer.
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, bmpData.Width, bmpData.Height, 0,
+                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, bmpData.Scan0);
+            
+            // Last, we unlock the bitmap. At this point, the Bitmap is no longer needed and can be discarded.
+            bmp.UnlockBits(bmpData);
+
+            // We also need to tell OpenGL how to render the texture when it is either smaller or larger than the
+            // original bitmap size. Here, we are using nearest as we want to see the pixels without any filtering.
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMinFilter.Nearest);
+
+            // Finally, we return the texture buffer handle back to the application for later use.
+            return id;
+        }
+    }
+}
